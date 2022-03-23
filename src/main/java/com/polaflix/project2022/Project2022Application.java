c0ea940d5ffc3239b498bcb764f0712117614860
alejuwilches <alejuwilches@gmail.com>
2022-03-23 11:01:59 +0100

    first commit

---
+++ src/main/java/com/polaflix/project2022/Project2022Application.java
@@ -0,0 +1,13 @@
+package com.polaflix.project2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/polaflix/project2022/Project2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polaflix.project2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
